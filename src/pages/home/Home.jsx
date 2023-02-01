6f34ba701a96b28c12a5ef4d8867bd7f6ff49b53
Sarah-Katz <Sarahkatz59@gmail.com>
2023-02-01 11:43:47 +0100

    Footer V1

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import Footer from "../../component/footer/footer"
+import Header from "../../component/header/Header"
+
+export default function Home() {
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    )
+}
