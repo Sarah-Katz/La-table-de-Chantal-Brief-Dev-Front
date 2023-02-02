c7fdcc3f516642983f1ff38b4d4e7b9d78dba232
Sarah-Katz <Sarahkatz59@gmail.com>
2023-02-02 10:57:34 +0100

    V1 home + slider

--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -4,8 +4,8 @@ export default function Footer() {
     return (
         <footer>
             <span className="footer-text">Retrouvez nous sur </span>
-            <a className="footer-link" href="https://www.facebook.com/LaTableDeChantal/" target="_blank" rel="noreferrer"><i class="fa-brands fa-facebook"></i></a>
-            <a className="footer-link" href="https://www.facebook.com/LaTableDeChantal/" target="_blank" rel="noreferrer"><i class="fa-brands fa-instagram"></i></a>
+            <a className="footer-link" href="https://www.facebook.com/LaTableDeChantal/" target="_blank" rel="noreferrer"><i className="fa-brands fa-facebook"></i></a>
+            <a className="footer-link" href="https://www.facebook.com/LaTableDeChantal/" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a>
         </footer>
     )
 }
